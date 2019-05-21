99dddf21ebb9cc6432552f854507cbe9134e9e0e
afshinfard <a.afshinfard@gmail.com>
2019-05-21 16:32:59 -0700

    updates

--- phypy_test/main.py
+++ phypy_test/main.py
@@ -5,27 +5,77 @@ from networkx.algorithms import community as nxcommunity
 import scipy as sp
 from sklearn.metrics.pairwise import cosine_similarity
 
-
-def split_subgraph_into_chunks_randomly(node_set, max_size=200):
-    "Split the subgraph into chunks for faster processing. Return chunks."
-    chunks_count = 1
-    if len(node_set) > max_size:
-        chunks_count = 1 + int(len(node_set) / 200)
-    ys = list(node_set)
-    random.shuffle(ys)
-    size, leftover = divmod(len(node_set), chunks_count)
-    chunks = [ys[0 + size * i: size * (i + 1)] for i in list(range(chunks_count))]
-    edge = size * chunks_count
-    for i in list(range(leftover)):
-        chunks[i % chunks_count].append(ys[edge + i])
-    chunk_sets = [set() for _ in range(len(chunks))]
-    for i, c in zip(range(len(chunks)), chunks):
-        chunk_sets[i].update(set(c))
-    return chunk_sets
+mport argparse
+import itertools
+import multiprocessing
+import os
+import random
+import re
+import statistics
+import sys
+import timeit
+from collections import Counter
 
 
-def community_detection_cosine_of_squared(g, node_set):
-    """Square the adjacency matrix and then use cosine similarity to detect communities. Return communities."""
+setup = '''
+from networkx.algorithms import community as nxcommunity
+import networkx as nx
+import numpy as np
+adj_numpy = np.array([
+    [0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+    [1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+    [1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+    [1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0],
+    [0, 0, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 1, 0],
+    [0, 0, 0, 1, 1, 0, 1, 1, 0, 0, 0, 0, 1, 1],
+    [0, 0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 0, 1, 0],
+    [0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1],
+    [0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 1, 0],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1],
+    [0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 0, 0, 1, 0]])
+sum(abs(adj_numpy - adj_numpy.T))
+u = 12
+g = nx.from_numpy_matrix(adj_numpy)
+sub_graph = g.subgraph(g.neighbors(u))
+node_set = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13}
+node_set = set(g.neighbors(12))
+'''
+print(min(timeit.Timer('a = g[u].keys()', setup=setup).repeat(7, 1000000)))
+print(min(timeit.Timer('a = set(g.neighbors(u))', setup=setup).repeat(7, 1000000)))
+
+
+
+
+def detect_communities_biconnected_components(g, node_set):
+    """Separate bi-connected components. Return components."""
+    cut_vertices = set(nx.articulation_points(g.subgraph(node_set)))
+    components = list(nx.connected_components(g.subgraph(node_set - cut_vertices)))
+    return components
+
+
+def detect_communities_k_clique(g, node_set, k=3):
+    """Apply k-clique community detection. Return communities."""
+    return list(nx.algorithms.community.k_clique_communities(g.subgraph(node_set), k))
+
+
+def detect_communities_louvain(g, node_set, init_communities=None):
+    """Apply Louvain community detection on a single component. Return communities."""
+    if len(node_set) < 2:
+        return []
+    import community as louvain
+    partition = louvain.best_partition(g.subgraph(node_set), init_communities)
+    return [{node for node in partition.keys() if partition[node] == com}
+            for com in set(partition.values())]
+
+
+def detect_communities_cosine_of_squared(g, node_set, squaring=True, threshold=0.75):
+    """
+    Square the adjacency matrix and then use cosine similarity to detect communities.
+    Return communities.
+    """
     import scipy as sp
     import numpy as np
     from sklearn.metrics.pairwise import cosine_similarity
@@ -33,9 +83,12 @@ def community_detection_cosine_of_squared(g, node_set):
     communities = []
     if len(node_set) > 1:
         adj_array = nx.adjacency_matrix(g.subgraph(node_set)).toarray()
-        new_adj = np.multiply(
-            cosine_similarity(
-                sp.linalg.blas.sgemm(1.0, adj_array, adj_array)) >= 0.75, adj_array)
+        if squaring:
+            new_adj = np.multiply(
+                cosine_similarity(
+                    sp.linalg.blas.sgemm(1.0, adj_array, adj_array)) >= threshold, adj_array)
+        else:
+            new_adj = np.multiply(cosine_similarity(adj_array) >= threshold, adj_array)
         edges_to_remove = np.argwhere(new_adj != adj_array)
         barcode_dict = dict(zip(range(len(node_set)), list(node_set)))
         edges_to_remove_barcode = [(barcode_dict[i], barcode_dict[j])
@@ -43,99 +96,103 @@ def community_detection_cosine_of_squared(g, node_set):
         sub_graph_copy = nx.Graph(g.subgraph(node_set))
         sub_graph_copy.remove_edges_from(edges_to_remove_barcode)
         cos_components = list(nx.connected_components(sub_graph_copy))
-        cos_components.sort(key=len, reverse=True)
         for com in cos_components:
-            if len(com) > 1:
-                communities.append(com)
+            communities.append(com)
     return communities
 
 
-def community_detection_louvain(g, node_set, init_communities=False):
-    """Apply Louvain community detection on a single component. Return communities."""
-    import community as louvain
-
-    if len(node_set) > 1:
-        if not init_communities:
-            partition = louvain.best_partition(g.subgraph(node_set))
-        else:
-            partition = louvain.best_partition(g.subgraph(node_set), init_communities)
-    #     communities = []
-    #     for com in set(partition.values()):
-    #         community_nodes = {nodes for nodes in partition.keys() if partition[nodes] == com}
-    #         if len(community_nodes) > 1:
-    #             communities.append(community_nodes)
-    # return communities
-    return [{nodes for nodes in partition.keys() if partition[nodes] == com} for com in set(partition.values())]
+def determine_molecules_bc_k_cliques(g, u):
+    """
+    Assign the neighbours of this vertex to molecules by
+    applying k-cliques community detection for each bi-connected component.
+    """
+    return [community
+            for bi_connected_component in
+            detect_communities_biconnected_components(g, set(g.neighbors(u)))
+            for community in
+            detect_communities_k_clique(g, bi_connected_component)]
 
 
-def community_detection_k_clique(g, node_set, k=3):
-    """Apply k-clique community detection. Return communities."""
-    if len(node_set) < 2:
-        return []
-    return [set(i) for i in nx.community.k_clique_communities(g.subgraph(node_set), k)]
+def determine_molecules_bc_louvain(g, u):
+    """
+    Assign the neighbours of this vertex to molecules by
+    applying louvain for each bi-connected component.
+    """
+    return [community
+            for bi_connected_component in
+            detect_communities_biconnected_components(g, set(g.neighbors(u)))
+            for community in
+            detect_communities_louvain(g, bi_connected_component)]
 
 
-def community_detection_biconnected_components(g, node_set):
-    """Separate bi-connected components. Return components."""
-    cut_vertices = set(nx.articulation_points(g.subgraph(node_set)))
-    components = list(nx.connected_components(g.subgraph(node_set - cut_vertices)))
-    components.sort(key=len, reverse=True)
-    return components
+def determine_molecules_bc_cosine_of_squared(g, u):
+    """
+    Assign the neighbours of this vertex to molecules by
+    applying cosine of squared of the adjacency matrix for each bi-connected component.
+    """
+    return [community
+            for bi_connected_component in
+            detect_communities_biconnected_components(g, set(g.neighbors(u)))
+            for community in
+            detect_communities_cosine_of_squared(g, bi_connected_component)]
 
 
-def split_subgraph_into_chunks_randomly(node_set, max_size=200):
-    "Split the subgraph into chunks for faster processing. Return chunks."
-    chunks_count = 1
-    if len(node_set) > max_size:
-        chunks_count = 1 + int(len(node_set) / 200)
+def determine_molecules_consensus(g, u):
+    """
+    Assign the neighbours of this vertex to molecules
+    by Applying a stack of different approaches.
+    """
+    communities = []
+    communities2 = []
+    for bi_connected_component in detect_communities_biconnected_components(g, set(g.neighbors(u))):
+        for comset in detect_communities_k_clique(g, bi_connected_component):
+            communities.extend(comset)
+    for community in communities:
+        for comset in detect_communities_cosine_of_squared(g, community, squaring=False, threshold=0.5):
+            communities2.extend(comset)
+    communities = []
+    for community2 in communities2:
+        for comset in detect_communities_cosine_of_squared(g, community2):
+            communities.extend(comset)
+    return communities
+    # communities2 = []
+    # for community in communities:
+    #     communities2.extend(detect_communities_louvain(g, community))
+    # return communities2
+    # return [community2
+    #         for bi_connected_component in
+    #         detect_communities_biconnected_components(g, set(g.neighbors(u)))
+    #         for community in
+    #         detect_communities_k_clique(g, bi_connected_component)
+    #         for community2 in
+    #         detect_communities_cosine_of_squared(g, community, squaring=False)
+    #         # for community3 in
+    #         # detect_communities_cosine_of_squared(g, community2)
+    #         # for community4 in
+    #         # detect_communities_louvain(g, community3)
+    #         ]
+
+
+def partition_subgraph_into_bins_randomly(node_set, max_size=40):
+    """
+    Partition the subgraph into bins randomly for faster processing. Return bins.
+    Warning: This function is not deterministic.
+    """
+    bins_count = 1 + len(node_set) // max_size
     node_list = list(node_set)
     random.shuffle(node_list)
-    size, leftover = divmod(len(node_set), chunks_count)
-    chunks = [node_list[0 + size * i: size * (i + 1)] for i in list(range(chunks_count))]
-    edge = size * chunks_count
-    for i in list(range(leftover)):
-        chunks[i % chunks_count].append(node_list[edge + i])
-    chunk_sets = [set() for _ in range(len(chunks))]
-    for i, c in zip(range(len(chunks)), chunks):
-        chunk_sets[i].update(set(c))
-    return chunk_sets
-
-def merge_communities(g, communities, node_set=0, strategy=1, mode=1):
-    """Merge communities if appropriate. """
-    if len(communities) == 1:
-        return communities
-    if strategy == 1:  # Merge ad-hoc
-        merge_network = nx.Graph()
-        for i, j in enumerate(communities):
-            merge_network.add_node(i)
-        for i, com1 in enumerate(communities):
-            for k, com2 in enumerate(communities):
-                if i < k:
-                    if mode == 1:  # disjoint input communities.
-                        if nx.number_of_edges(
-                                g.subgraph(com1.union(com2))) - \
-                                nx.number_of_edges(g.subgraph(com1)) - \
-                                nx.number_of_edges(g.subgraph(com2)) > 8:
-                            merge_network.add_edge(i, k)
-                    else:  # overlapping input communities.
-                        if nx.number_of_edges(
-                                g.subgraph(com1.union(com2))) - \
-                                len(set(g.subgraph(com1).edges()).union(
-                                    set(g.subgraph(com2).edges()))) \
-                                > 8:
-                            merge_network.add_edge(i, k)
-        # res = []
-        # for i in list(nx.connected_components(merge_network)):
-        #     subset = set()
-        #     for j in list(i):
-        #         for barcode in list(communities[j]):
-        #             subset.add(barcode)
-        #     res.append(subset)
-        # return res
-        return [{barcode for j in i for barcode in communities[j]}
-                for i in nx.connected_components(merge_network)]
-    # Merge by Initializing Louvain with the communities
-    return community_detection_louvain(g, node_set, communities)
+    size, leftover = divmod(len(node_set), bins_count)
+    bins = [node_list[0 + size * i: size * (i + 1)] for i in range(bins_count)]
+    edge = size * bins_count
+    for i in range(leftover):
+        bins[i % bins_count].append(node_list[edge + i])
+    print(bins)
+    return [set(x) for x in bins]
+    # bin_sets = [set() for _ in bins]
+    # for i, c in enumerate(bins):
+    #     bin_sets[i].update(c)
+    #     print(c)
+    # return bin_sets
 
 communities = []
 for bi_connected_component in community_detection_biconnected_components(g, set(g.neighbors(u))):
@@ -173,7 +230,7 @@ for bi_connected_component in community_detection_biconnected_components(g, set(
     sub_communities = []
     for chunk in split_subgraph_into_chunks_randomly(bi_connected_component, max_size=50):
         sub_communities += community_detection_k_clique(g, chunk, 3)
-        # for chunk_community in Physlr.community_detection_k_clique(g, chunk, 3):
+        # for chunk_community in community_detection_k_clique(g, chunk, 3):
         #     sub_communities.append(chunk_community)
     print(sub_communities)
     communities += merge_communities(g, sub_communities, bi_connected_component, strategy=1)
@@ -324,11 +381,11 @@ def determine_safer_backbones_of_trees(g, pruning_threshold):
     paths = []
     for component in nx.connected_components(g):
         gcomponent = g.subgraph(component)
-        messages = Physlr.determine_reachability_of_tree_by_message_passing(gcomponent)
-        gcomponents = Physlr.detect_and_cut_junctions_of_tree(gcomponent, messages, pruning_threshold)
+        messages = determine_reachability_of_tree_by_message_passing(gcomponent)
+        gcomponents = detect_and_cut_junctions_of_tree(gcomponent, messages, pruning_threshold)
         for component2 in nx.connected_components(gcomponents):
             gcomponent2 = g.subgraph(component2)
-            u, v, _ = Physlr.diameter_of_tree(gcomponent2, weight="n")
+            u, v, _ = diameter_of_tree(gcomponent2, weight="n")
             path = nx.shortest_path(gcomponent2, u, v, weight="n")
             paths.append(path)
     paths.sort(key=len, reverse=True)
@@ -342,7 +399,7 @@ def determine_safer_backbones(g, pruning_threshold=100):
     backbones = []
     while not nx.is_empty(g):
         gmst = g
-        paths = Physlr.determine_safer_backbones_of_trees(gmst, pruning_threshold)
+        paths = determine_safer_backbones_of_trees(gmst, pruning_threshold)
         backbones.extend(paths)
         vertices = [u for path in paths for u in path]
         neighbors = [v for u in vertices for v in g.neighbors(u)]
@@ -432,3 +489,92 @@ def determine_junctions(g, junction_threshold=100):
     gmst = nx.maximum_spanning_tree(g, weight="n")
     junctions = determine_junctions_of_trees(gmst, junction_threshold)
     return junctions
+
+def detect_communities_biconnected_components(g, node_set):
+    """Separate bi-connected components. Return components."""
+    cut_vertices = set(nx.articulation_points(g.subgraph(node_set)))
+    components = list(nx.connected_components(g.subgraph(node_set - cut_vertices)))
+    return components
+
+def detect_communities_k_clique(g, node_set, k=3):
+    """Apply k-clique community detection. Return communities."""
+    return list(nx.algorithms.community.k_clique_communities(g.subgraph(node_set), k))
+
+def determine_molecules_bc_k_cliques(g, u):
+    """
+    Assign the neighbours of this vertex to molecules by
+    applying k-cliques community detection for each bi-connected component.
+    """
+    return [community
+            for bi_connected_component in
+            detect_communities_biconnected_components(g, set(g.neighbors(u)))
+            for community in
+            detect_communities_k_clique(g, bi_connected_component)]
+
+def determine_molecules_consensus(g, u):
+    """
+    Assign the neighbours of this vertex to molecules
+    by Applying a queue of different algorithms on top of each other.
+    """
+    from collections import deque
+
+    communities = [g[u].keys()]
+    communities_final = []
+
+    alg_list = deque(["bc", "k3", "bc"])
+    while alg_list:
+        communities_final.clear()
+        algoritm = alg_list.popleft()
+        if algoritm == "bc":
+            for component in communities:
+                communities_final.extend(
+                    detect_communities_biconnected_components(g, component))
+        elif algoritm == "k3":
+            for component in communities:
+                communities_final.extend(
+                    detect_communities_k_clique(g, component))
+        elif algoritm == "cos":
+            for component in communities:
+                communities_final.extend(
+                    detect_communities_cosine_of_squared(
+                        g, component, squaring=False, threshold=0.4))
+        elif algoritm == "sqCos":
+            for component in communities:
+                communities_final.extend(
+                    detect_communities_cosine_of_squared(g, component))
+        # for component in communities_final:
+        #     print(component)
+        communities = communities_final.copy()
+        #communities.clear()
+        #communities.extend(communities_final)
+    return communities_final
+
+adj_numpy = np.array([
+    [0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+    [1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+    [1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+    [1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0],
+    [0, 0, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 1, 0],
+    [0, 0, 0, 1, 1, 0, 1, 1, 0, 0, 0, 0, 1, 1],
+    [0, 0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 0, 1, 0],
+    [0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1],
+    [0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 1, 0],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1],
+    [0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 0, 0, 1, 0]])
+sum(abs(adj_numpy - adj_numpy.T))
+u = 12
+g = nx.from_numpy_matrix(adj_numpy)
+sub_graph = g.subgraph(g.neighbors(u))
+node_set = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13}
+node_set = set(g.neighbors(12))
+
+communities =[]
+for bi_connected_component in detect_communities_biconnected_components(g, g[u].keys()):
+    # print(bi_connected_component)
+    communities.extend(detect_communities_cosine_of_squared(g, bi_connected_component, squaring=False, threshold=0.5))
+print(communities)
+    # for comset in detect_communities_biconnected_components(g, bi_connected_component):
+    #    communities.extend(comset)
