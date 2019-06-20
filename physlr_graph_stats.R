585081b47e701a77d60796252523d8fcf4723a99
afshinfard <a.afshinfard@gmail.com>
2019-06-20 09:49:44 -0700

    updates

--- physlr_graph_stats.R
+++ physlr_graph_stats.R
@@ -10,7 +10,10 @@ dat="/projects/btl_scratch/aafshinfard/physlr2/physlr/data/hg004.indexlr.n100-50
 
 
 a = read.table(dat, header = TRUE, fill = TRUE)
+a[1:10,"Comp.count"]=0
+
 head(a)
-ggplot(data = a, aes(x=Threshold, y = Comp.count)) + geom_step()
+ggplot(data = a, aes(x=Threshold, y = Comp.count)) + geom_col()
 ggplot(data = a, aes(x=Threshold, y = Node.count)) + geom_col()
 ggplot(data = a, aes(x=Threshold, y = Edge.count)) + geom_col()
+
