3b69cd1e5ef4baeee073a4ce998bd05376182713
afshinfard <a.afshinfard@gmail.com>
2019-08-06 12:22:40 -0700

    updates

--- physlr_graph_stats.R
+++ physlr_graph_stats.R
@@ -3,10 +3,11 @@ library("ggplot2")
 library("igraph")
 
 
-dat="/projects/btl_scratch/aafshinfard/physlr2/physlr/data/hg004.indexlr.n100-5000.c2-x.physlr.overlap.n10.2.stats"
+dat="/projects/btl_scratch/aafshinfard/projects/physlr2/physlr/data/hg004.indexlr.n100-5000.c2-x.physlr.overlap.n10.2.stats"
 
 a = read.table(dat, header = TRUE, fill = TRUE)
 a[1:10,"Comp.count"]=0
+
 
 head(a)
 ggplot(data = a, aes(x=Threshold, y = Comp.count)) + geom_col() + labs(x="n, g, w, or whatever :D", y="Number of components (in the overlap graph)")
