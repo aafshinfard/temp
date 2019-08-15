49f338e5587f5f0a9760e1c773bf44b4f0f63d81
afshinfard <a.afshinfard@gmail.com>
2019-08-15 10:21:09 -0700

    updates

--- Physlr_plot_histo_param_stats.R
+++ Physlr_plot_histo_param_stats.R
@@ -6,9 +6,12 @@
 # then 
 library("ggplot2")
 data<-read.csv("/projects/btl_scratch/aafshinfard/projects/physlr2/physlr/data/hg004.k32-w32.n100-5000.c2-x.physlr.overlap.edge_weights", sep = "\t" , header=T)
+#Jonathans output:
+data<-read.csv("/projects/btl/jowong/github/physlr/data/histogram.txt", sep = "\t" , header=T)
+
 head(data)
 class(data[1,1])
-hist(data[,1])
+#hist(data[,1])
 dim(data)
 df = data.frame(data)
 head(df)
@@ -16,4 +19,5 @@ sub_df = data.frame(df[sample(nrow(df), 1000000), ])
 class(sub_df)
 names(sub_df) = "edge_weight"
 head(sub_df)
-p = ggplot(sub_df, aes(x=edge_weight))+geom_histogram(binwidth = 1)+xlim(0,100)
+p = ggplot(sub_df, aes(x=edge_weight))+geom_histogram(binwidth = 1)#+xlim(0,100)
+p10 = ggplot(round(sub_df/10), aes(x=edge_weight))+geom_histogram(binwidth = 1)#+xlim(0,100)
