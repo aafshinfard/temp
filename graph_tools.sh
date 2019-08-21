1b8f16a3ce1854c42188bbeafd9060e1e14c12c8
afshinfard <a.afshinfard@gmail.com>
2019-08-21 16:46:10 -0700

    updates

--- graph_tools.sh
+++ graph_tools.sh
@@ -1,5 +1,49 @@
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+file="f1chr4.k32-w32.n100-1000.c2-x.physlr.overlap.n15000.2.tsv"
+cat f1chr4.k32-w32.n100-1000.c2-x.physlr.overlap.n15000.3.tsv | awk '{ if (NF>3) {print $1"\t"$2"\t"$3} else if (1) {print $1"\t"$2}}' > f1chr4.k32-w32.n100-1000.c2-x.physlr.overlap.n15000.nisn.tsv
+cat f1chr4.k32-w32.n100-1000.c2-x.physlr.overlap.n15000.3.tsv | awk '{ if (NF>3) {print $1"\t"$2"\t"$4} else if (1) {print $1"\t"$2}}' > f1chr4.k32-w32.n100-1000.c2-x.physlr.overlap.n15000.nisW.tsv
+
+
+
+
+
+
+
+
+
+
+
  
 
 ccomps #for connected components of the graph
