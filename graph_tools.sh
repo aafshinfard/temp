439295c3aa4598dabc098186ead7833133f61a6a
afshinfard <a.afshinfard@gmail.com>
2019-08-22 10:18:13 -0700

    updates

--- graph_tools.sh
+++ graph_tools.sh
@@ -30,14 +30,14 @@
 
 
 file="f1chr4.k32-w32.n100-1000.c2-x.physlr.overlap.n15000.2.tsv"
-cat f1chr4.k32-w32.n100-1000.c2-x.physlr.overlap.n15000.2.tsv | awk '{ if (NF>3) {print $1"\t"$2"\t"$3} else if (1) {print $1"\t"$2}}' > f1chr4.k32-w32.n100-1000.c2-x.physlr.overlap.n15000.nisn.tsv
-cat f1chr4.k32-w32.n100-1000.c2-x.physlr.overlap.n15000.2.tsv | awk '{ if (NF>3) {print $1"\t"$2"\t"$4} else if (1) {print $1"\t"$2}}' > f1chr4.k32-w32.n100-1000.c2-x.physlr.overlap.n15000.nisW.tsv
+cat f1chr4.k32-w32.n100-1000.c2-x.physlr.overlap.n15000.tsv | awk '{ if (NF>3) {print $1"\t"$2"\t"$3} else if (NF==2) {print $1"\t"$2} else if (1) print}' > f1chr4.k32-w32.n100-1000.c2-x.physlr.overlap.n15000.nisn.tsv
+cat f1chr4.k32-w32.n100-1000.c2-x.physlr.overlap.n15000.tsv | awk '{ if (NF>3) {print $1"\t"$2"\t"$4} else if (NF==2) {print $1"\t"$2} else if (1) print}' > f1chr4.k32-w32.n100-1000.c2-x.physlr.overlap.n15000.nisW.tsv
 
 
 nohup make f1chr4.k32-w32.n100-1000.c2-x.physlr.overlap.n15000.nisW.gv &
 nohup make f1chr4.k32-w32.n100-1000.c2-x.physlr.overlap.n15000.nisn.gv &
 
- 
+cat f1chr4.k32-w32.n100-1000.c2-x.physlr.overlap.n15000.nisn.tsv | awk 'NF==0 {print s; print; getline; print} {s=$1$2}'
 
 ccomps #for connected components of the graph
 ccomps -xz 
