40cd6f54705d3560042c226a6a767227c189e764
afshinfard <a.afshinfard@gmail.com>
2019-09-03 09:38:40 -0700

    updates

--- physlr_multi_param_edge_cleaning.R
+++ physlr_multi_param_edge_cleaning.R
@@ -1,4 +1,5 @@
 library(ggplot2)
+library("Rtsne")
 #############################################################
 ### plot 2 features (n and w)
 
@@ -53,3 +54,50 @@ head(sub_data2)
 ggplot(sub_data2, aes(x = n_tfidf, y = w_tfidf, colour = label)) +
   geom_point() + ggtitle("f1chr4 n vs. w (with weighted minimizers)") + xlab("n_tfidf") + ylab("w_tfidf")+ theme(text = element_text(size=16))
 + facet_wrap( ~ V5)
+head(as.matrix(sub_data2[,1:9]))
+head((sub_data2[,1:9]))
+
+###############################################
+# PCA
+pca = prcomp(sub_data2[,3:9], scale=TRUE)
+plot(pca$x[,1], pca$x[,2])
+pca.var = pca$sdev^2
+pca.var.per = round(pca.var/sum(pca.var)*100,1)
+barplot(pca.var,main="Scree plot", xlab="PC", ylab="Variation (%)")
+
+pca.data = data.frame(Sample=rownames(pca$x),
+                      X=pca$x[,1],
+                      Y=pca$x[,2])
+head(pca.data)
+ggplot(data=pca.data, aes(x=X, y=Y, label=Sample))+
+  geom_point()+#geom_text() +
+  xlab(paste("PC1 - ",pca.var.per[1], "%", sep=""))+
+  ylab(paste("PC2 - ",pca.var.per[2], "%", sep=""))+
+  theme_bw()+
+  ggtitle("PCA")
+head(pca.data)
+head(sub_data2)
+pca.data.labeled = cbind(pca.data,sub_data2[,10])
+names(pca.data.labeled)[4] = "label"
+head(pca.data.labeled)
+ggplot(data=pca.data.labeled, aes(x=X, y=Y, colour=label))+
+  geom_point()+#geom_text() +
+  xlab(paste("PC1 - ",pca.var.per[1], "%", sep=""))+
+  ylab(paste("PC2 - ",pca.var.per[2], "%", sep=""))+
+  theme_bw()+
+  ggtitle("PCA")
+
+###################################################
+# t-SNE
+head(iris)
+iris_unique <- unique(iris) # Remove duplicates
+iris_matrix <- as.matrix(iris_unique[,1:4])
+set.seed(42) # Set a seed if you want reproducible results
+tsne_out <- Rtsne(iris_matrix) # Run TSNE
+plot(tsne_out$Y,col=iris_unique$Species)
+
+head(sub_data2[,3:10])
+
+mat = as.matrix(sub_data2[,3:9])
+tsne_out <- Rtsne(mat, pca_scale=TRUE) # Run TSNE
+plot(tsne_out$Y,col=sub_data2$label)
