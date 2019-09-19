f33b59b4f1b4a3870e5de658769c39dfae1eaf07
afshinfard <a.afshinfard@gmail.com>
2019-09-19 10:14:32 -0700

    updates

--- physlr_multi_param_edge_cleaning.R
+++ physlr_multi_param_edge_cleaning.R
@@ -114,6 +114,43 @@ ggplot(pca.data.labeled, aes(x=X, y=Y, colour=label)) +
 ggplot(pca.data.labeled, aes(x=X, y=Y)) +
   stat_density_2d(aes(fill = stat(level)), geom = "polygon") + ggtitle("PCA - f1chr4 - true and false edges") + xlab("x") + ylab("y")+ theme(text = element_text(size=16)) + facet_wrap( ~ sub_data2$label)
 
+##### unb:
+pca = prcomp(sub_data2[,3:9], scale=TRUE)
+plot(pca$x[,1], pca$x[,2])
+pca.var = pca$sdev^2
+pca.var.per = round(pca.var/sum(pca.var)*100,1)
+barplot(pca.var,main="Scree plot", xlab="PC", ylab="Variation (%)")
+
+pca.data = data.frame(Sample=rownames(pca$x),
+                      X=pca$x[,1],
+                      Y=pca$x[,2])
+head(pca.data)
+ggplot(data=pca.data, aes(x=X, y=Y, label=Sample))+
+  geom_point()+#geom_text() +
+  xlab(paste("PC1 - ",pca.var.per[1], "%", sep=""))+
+  ylab(paste("PC2 - ",pca.var.per[2], "%", sep=""))+
+  theme_bw()+
+  ggtitle("PCA")
+head(pca.data)
+head(sub_data2)
+pca.data.labeled = cbind(pca.data,sub_data2[,'label'])
+names(pca.data.labeled)[4] = "label"
+head(pca.data.labeled)
+ggplot(data=pca.data.labeled, aes(x=X, y=Y, colour=label))+
+  geom_point()+#geom_text() +
+  xlab(paste("PC1 - ",pca.var.per[1], "%", sep=""))+
+  ylab(paste("PC2 - ",pca.var.per[2], "%", sep=""))+
+  theme_bw()+
+  ggtitle("PCA")
+
+
+ggplot(pca.data.labeled, aes(x=X, y=Y, colour=label)) +
+  geom_point() + ggtitle("PCA - f1chr4 - true and false edges") + xlab("x") + ylab("y")+ theme(text = element_text(size=16)) + facet_wrap( ~ sub_data2$label)
+ggplot(pca.data.labeled, aes(x=X, y=Y)) +
+  stat_density_2d(aes(fill = stat(level)), geom = "polygon") + ggtitle("PCA - f1chr4 - true and false edges") + xlab("x") + ylab("y")+ theme(text = element_text(size=16)) + facet_wrap( ~ sub_data2$label)
+
+
+
 ###################################################
 # t-SNE
 head(iris)
