9db4e9d4a608b3f3ea412bf36d407f1459b2e86d
afshinfard <a.afshinfard@gmail.com>
2020-01-15 11:16:49 -0800

    debugging

--- tests.cpp
+++ tests.cpp
@@ -171,7 +171,7 @@ inline double cosine_similarity_vectors(adjacencyMatrix_t::iterator& row_i, adja
 
 inline
 adjacencyMatrix_t
-calculate_cosine_similarity_2d_v2(adjacencyMatrix_t& adj_mat, vector<vector<double> >& cosimilarity)
+calculate_cosine_similarity_2d_v2(adjacencyMatrix_t adj_mat, vector<vector<double> > cosimilarity)
 {
     // Assumptions: the input matrix is symmetric and cubic
     // This function calculate the 2-dimensional cosine similarity of the input matrix
