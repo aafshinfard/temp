093ef6c14d0e039350a75e6bc8a4eac8f990abe3
Amirhossein Afshinfard <31241012+afshinfard@users.noreply.github.com>
2020-03-31 10:36:51 -0700

    Update physlr_subgraph_visualize.sh

--- physlr_subgraph_visualize.sh
+++ physlr_subgraph_visualize.sh
@@ -8,6 +8,11 @@ grep '_10\s' vertices_hg004.k40-w32.n100-5000.c2-x.physlr.overlap.m85.mol.tsv | 
 grep '_10\s' vertices_hg004.k40-w32.n100-5000.c2-x.physlr.overlap.m85.mol.tsv > molecules_10_raw.tsv
 cat molecules_10_raw.tsv | awk '{print $1}' > molecules_10_raw2.tsv
 mv  molecules_10_raw2.tsv  molecules_10_raw.tsv
+cat molecules_10_raw.tsv | sed -e "s/$*_10$//" > molecules_10_barcode.tsv
+
+***exclude_source=1***
+make hg004.k40-w32.n100-5000.c2-x.physlr.overlap.subgraphs d=10 v="$(paste -d, -s molecules_10_barcode.tsv)"
+make hg004.k40-w32.n100-5000.c2-x.physlr.overlap.m85.subgraphs d=10 v="$(paste -d, -s molecules_10_barcode.tsv)"
 make hg004.k40-w32.n100-5000.c2-x.physlr.overlap.m85.mol.subgraphs d=10 v="$(paste -d, -s molecules_10_raw.tsv)"
 
 
