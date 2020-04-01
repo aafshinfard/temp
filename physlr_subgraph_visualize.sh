24fd9df18decfc64862778a7578f21ad7f348aea
Amirhossein Afshinfard <31241012+afshinfard@users.noreply.github.com>
2020-03-31 23:16:16 -0700

    Update physlr_subgraph_visualize.sh

--- physlr_subgraph_visualize.sh
+++ physlr_subgraph_visualize.sh
@@ -10,11 +10,16 @@ cat molecules_10_raw.tsv | awk '{print $1}' > molecules_10_raw2.tsv
 mv  molecules_10_raw2.tsv  molecules_10_raw.tsv
 cat molecules_10_raw.tsv | sed -e "s/$*_10$//" > molecules_10_barcode.tsv
 
-***exclude_source=1***
+
 make hg004.k40-w32.n100-5000.c2-x.physlr.overlap.subgraphs d=10 v="$(paste -d, -s molecules_10_barcode.tsv)"
 make hg004.k40-w32.n100-5000.c2-x.physlr.overlap.m85.subgraphs d=10 v="$(paste -d, -s molecules_10_barcode.tsv)"
 make hg004.k40-w32.n100-5000.c2-x.physlr.overlap.m85.mol.subgraphs d=10 v="$(paste -d, -s molecules_10_raw.tsv)"
 
+make hg004.k40-w32.n100-5000.c2-x.physlr.overlap.m85.subgraphs/....gv min_component_size=0 -n (use it in the follwing commands)
+cd hg004.k40-w32.n100-5000.c2-x.physlr.overlap.m85.subgraphs
+for i in *.tsv; do env PYTHONPATH=/projects/btl_scratch/aafshinfard/projects/physlr/publication/jowong/hg004/physlr /projects/btl/aafshinfard/virtuEnv/pypy3/bin/pypy3 /projects/btl_scratch/aafshinfard/projects/physlr/publication/jowong/hg004/physlr/bin/physlr filter --min-component-size=0 -Ogv -V1 $i > "$i.gv"; done
+for i in *.tsv.gv; do [[[making eps using command for sfdp.pdf]]]
+for i in *.tsv.gv.eps; do convert $i "$i.pdf"
 
 
 
