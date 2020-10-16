d7259f05abd601934408b4a295705cd53c944a0e
Amirhossein Afshinfard <31241012+afshinfard@users.noreply.github.com>
2020-10-17 02:22:31 +0330

    Create na12878_runJupiter.sh

---
+++ na12878_runJupiter.sh
@@ -0,0 +1,16 @@
+export PATH="/projects/btl/lcoombe/miniconda3/envs/jupiter/bin:$PATH"
+
+./jupiter ng=95 maxScaff=40 t=16 name=ref.VS.na12878.ONT.shasta.ng95.maxScaff40 ref=grch38.fa fa=na12878.ONT.shasta.fa
+./jupiter ng=95 maxScaff=40 t=16 name=ref.VS.na12878.ONT.shasta.arcs.ng95.maxScaff40 ref=grch38.fa fa=na12878.ONT.shasta.arcs.fa
+./jupiter ng=95 maxScaff=40 t=16 name=ref.VS.na12878.ONT.shasta.physlr.ng95.maxScaff40 ref=grch38.fa fa=na12878.ONT.shasta.physlr.fa
+
+./jupiter ng=95 maxScaff=40 t=16 name=ref.VS.na12878.pacbio.ng95.maxScaff40 ref=grch38.fa fa=na12878.pacbio.fa
+./jupiter ng=95 maxScaff=40 t=16 name=ref.VS.na12878.pacbio.arcs.ng95.maxScaff40 ref=grch38.fa fa=na12878.pacbio.arcs.fa
+./jupiter ng=95 maxScaff=40 t=16 name=ref.VS.na12878.pacbio.physlr.ng95.maxScaff40 ref=grch38.fa fa=na12878.pacbio.physlr.fa
+
+./jupiter ng=95 maxScaff=40 t=16 name=ref.VS.na12878.pe_mpet.abyss.ng95.maxScaff40 ref=grch38.fa fa=na12878.pe_mpet.abyss.fa
+./jupiter ng=95 maxScaff=40 t=16 name=ref.VS.na12878.pe_mpet.abyss.arcs.ng95.maxScaff40 ref=grch38.fa fa=na12878.pe_mpet.abyss.arcs.fa
+./jupiter ng=95 maxScaff=40 t=16 name=ref.VS.na12878.pe_mpet.abyss.physlr.ng95.maxScaff40 ref=grch38.fa fa=na12878.pe_mpet.abyss.physlr.fa
+
+./jupiter ng=95 maxScaff=40 t=16 name=ref.VS.na12878.stLFR.supernova.ng95.maxScaff40 ref=grch38.fa fa=na12878.stLFR.supernova.fa
+./jupiter ng=95 maxScaff=40 t=16 name=ref.VS.na12878.stLFR.supernova.physlr.ng95.maxScaff40 ref=grch38.fa fa=na12878.stLFR.supernova.physlr.fa
